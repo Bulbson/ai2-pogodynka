025383a832ac1f9504ec0dc63d7d4c348e389fcd
Hubert Pluchrat <ph49872>
2024-02-03 18:48:31 +0100

    lab-d

--- pogodynka/src/Controller/WeatherController.php
+++ pogodynka/src/Controller/WeatherController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Location;
 use App\Repository\MeasurementRepository;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
